6ad8387430fbe223cb00bdcac493c3fc2ee14c98
eungit <dmsrudqmdl328@gmail.com>
2025-01-15 11:16:05 +0900

    js 실습2.콜백함수

--- JS/04.js
+++ JS/04.js
@@ -1,4 +1,5 @@
 //콜백 함수
+//함수의 인자로 전달
 function completeTask() {
     console.log('completeTask 함수 시작');
 }
@@ -19,3 +20,14 @@ const applyOperation = (func, number) => {//3번
 const double = x => x * 2;//1번
 
 console.log(applyOperation(double, 10))//2번 -> 5번
+
+
+//함수의 반환값으로 사용
+const makeMultiplier = (n) => {
+    return (x) => x * n;
+}
+const doubleNumber = makeMultiplier(2);
+const tripleNumber = makeMultiplier(3);
+
+console.log(doubleNumber(5));
+console.log(tripleNumber(5));
